fe17052024274796d3fd23951a3d11d4b892d243
Abel-Git2103 <abel_jornet@hotmail.com>
2022-02-25 20:19:31 +0100

    MENSAJE DTO Y SEUNE DTO MODIFICADOS

--- src/main/java/main/dto/Mensaje.java
+++ src/main/java/main/dto/Mensaje.java
@@ -1,11 +1,99 @@
 package main.dto;
 
+import java.sql.Time;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "mensaje")
+@Table(name = "Mensaje")
 public class Mensaje {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String contenido;
+	private Time hora;
+
+	@ManyToOne
+	@JoinColumn(name = "Id_chat")
+	Chat chat;
+
+	@ManyToOne
+	@JoinColumn(name = "Nombre_usuario")
+	Usuario usuario;
+
+	// CONSTRUCTORES
+	public Mensaje() {
+	}
+
+	/**
+	 * @param id
+	 * @param contenido
+	 * @param hora
+	 * @param chat
+	 * @param usuario
+	 */
+	public Mensaje(int id, String contenido, Time hora, Chat chat, Usuario usuario) {
+		this.id = id;
+		this.contenido = contenido;
+		this.hora = hora;
+		this.chat = chat;
+		this.usuario = usuario;
+	}
+
+	// GETTERS Y SETTERS
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+
+	public Time getHora() {
+		return hora;
+	}
+
+	public void setHora(Time hora) {
+		this.hora = hora;
+	}
+
+	public Chat getChat() {
+		return chat;
+	}
+
+	public void setChat(Chat chat) {
+		this.chat = chat;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	// METODO TO STRING
+	@Override
+	public String toString() {
+		return "Mensaje [id=" + id + ", contenido=" + contenido + ", hora=" + hora + ", chat=" + chat + ", usuario="
+				+ usuario + "]";
+	}
+
 }
--- src/main/java/main/dto/SeUne.java
+++ src/main/java/main/dto/SeUne.java
@@ -1,5 +1,73 @@
 package main.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SeUne")
 public class SeUne {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@ManyToOne
+	@JoinColumn(name = "Id_grupo")
+	Grupo grupo;
+
+	@ManyToOne
+	@JoinColumn(name = "Nombre_usuario")
+	Usuario usuario;
+
+	// CONSTRUCTORES
+	public SeUne() {
+	}
+
+	/**
+	 * @param id
+	 * @param grupo
+	 * @param usuario
+	 */
+	public SeUne(int id, Grupo grupo, Usuario usuario) {
+		this.id = id;
+		this.grupo = grupo;
+		this.usuario = usuario;
+	}
+
+	// GETTERS Y SETTERS
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Grupo getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(Grupo grupo) {
+		this.grupo = grupo;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	// METODO TO STRING
+	@Override
+	public String toString() {
+		return "SeUne [id=" + id + ", grupo=" + grupo + ", usuario=" + usuario + "]";
+	}
 
 }
