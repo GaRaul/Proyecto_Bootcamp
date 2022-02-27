65454d131f57d017f6e62f89ee7beb9ce7c159b2
Abel-Git2103 <abel_jornet@hotmail.com>
2022-02-27 14:24:03 +0100

    SERVICE SeUne - "Finalizado"

--- src/main/java/main/service/SeUneServiceImpl.java
+++ src/main/java/main/service/SeUneServiceImpl.java
@@ -1,5 +1,42 @@
 package main.service;
 
-public class SeUneServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import main.dao.ISeUneDao;
+import main.dto.SeUne;
+
+@Service
+public class SeUneServiceImpl implements ISeUneService {
+
+	@Autowired
+	ISeUneDao iSeUneDao;
+
+	@Override
+	public List<SeUne> listarSeUne() {
+		return iSeUneDao.findAll();
+	}
+
+	@Override
+	public SeUne guardarSeUne(SeUne seUne) {
+		return iSeUneDao.save(seUne);
+	}
+
+	@Override
+	public SeUne seUneXID(Integer id) {
+		return iSeUneDao.findById(id).get();
+	}
+
+	@Override
+	public SeUne actualizarSeUne(SeUne seUne) {
+		return iSeUneDao.save(seUne);
+	}
+
+	@Override
+	public void eliminarSeUne(Integer id) {
+		iSeUneDao.deleteById(id);
+	}
 
 }
