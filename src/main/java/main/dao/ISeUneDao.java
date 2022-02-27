04ed044bba052ab0655a6a2ac74dcc21cef02069
Abel-Git2103 <abel_jornet@hotmail.com>
2022-02-27 14:23:10 +0100

    DAO SeUne - "Finalizado"

---
+++ src/main/java/main/dao/ISeUneDao.java
@@ -0,0 +1,9 @@
+package main.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import main.dto.SeUne;
+
+public interface ISeUneDao extends JpaRepository<SeUne, Integer> {
+
+}
