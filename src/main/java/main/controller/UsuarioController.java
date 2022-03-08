7abe35cde9b330868f1d5a564a7a7e98edc6842f
Abel-Git2103 <abel_jornet@hotmail.com>
2022-03-08 13:48:16 +0100

    CORRECCIÓN USUARIO CONTROLLER

--- src/main/java/main/controller/UsuarioController.java
+++ src/main/java/main/controller/UsuarioController.java
@@ -60,7 +60,7 @@ public class UsuarioController {
 		usuario_seleccionado.setPassword(usuario.getPassword());
 		usuario_seleccionado.setFotoPerfil(usuario.getFotoPerfil());
 		usuario_seleccionado.setFecha_creación(usuario.getFecha_creación());
-		usuario_seleccionado.setFecha_modificacion(usuario.getFecha_creación());
+		usuario_seleccionado.setFecha_modificacion(usuario.getFecha_modificacion());
 		usuario_seleccionado.setRol(usuario.getRol());
 		usuario_seleccionado.setUsuarioSteam(usuario.getUsuarioSteam());
 		usuario_seleccionado.setGrupo(usuario.getGrupo());
