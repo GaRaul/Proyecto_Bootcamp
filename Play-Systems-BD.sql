0ffa6a49975920d30a9aed4f30fae1c764163154
Abel-Git2103 <97507570+Abel-Git2103@users.noreply.github.com>
2022-03-08 13:29:55 +0100

    Add files via upload

--- Play-Systems-BD.sql
+++ Play-Systems-BD.sql
@@ -2,12 +2,15 @@ CREATE DATABASE heroku_17d9f263de3e5e0;
 
 USE heroku_17d9f263de3e5e0;
 
+SET auto_increment_increment = 1;
+
 create table Juego (
-Id_juego int unsigned auto_increment primary key,
-Nombre varchar(100) unique,
-Descripcion varchar(500) not null,
-Imagen varchar(255),
-Genero varchar(100));
+Id_juego int(10) AUTO_INCREMENT PRIMARY KEY,
+Nombre varchar(100) DEFAULT NULL,
+Descripcion varchar(500) DEFAULT NULL,
+Imagen varchar(255) DEFAULT NULL,
+Genero varchar(100) DEFAULT NULL
+);
 
 insert into Juego (Nombre, Descripcion, Imagen, Genero) values ('League of Legends', '2 equipos de 5 jugadores pelean por destruir el Nexo enemigo', 'https://www.leagueoflegends.com/static/placeholder-1c66220c6149b49352c4cf496f70ad86.jpg', 'MOBA');
 insert into Juego (Nombre, Descripcion, Imagen, Genero) values ('Elden Ring', 'Juego RPG de fantasía oscura', 'https://i.redd.it/3e2afpjsi4f61.png', 'ARPG');
@@ -15,20 +18,24 @@ insert into Juego (Nombre, Descripcion, Imagen, Genero) values ('Valorant', 'Sho
 insert into Juego (Nombre, Descripcion, Imagen, Genero) values ('Mario Kart', 'Juego de conducción', 'https://upload.wikimedia.org/wikipedia/commons/3/30/Mario_kart_first_logo.png', 'CONDUCCIÓN');
 insert into Juego (Nombre, Descripcion, Imagen, Genero) values ('Age of Empires IV', 'Juego de estrategia medieval', 'https://i.blogs.es/50a9b3/age-of-empires-iv/1366_2000.jpg', 'ESTRATEGIA');
 
+select * from Juego;
+
 create table Grupo (
-Id_grupo int unsigned auto_increment primary key,
-Id_juego int unsigned,
+Id_grupo int auto_increment primary key,
+Id_juego int,
 Modo_de_Juego enum('Coop', 'PvP', 'PvE'),
 Dimension int unsigned default 2,
 Descripcion varchar(500),
 Juego_nuevo varchar(100),
 foreign key (Id_juego) references Juego(Id_juego) ON DELETE CASCADE ON UPDATE CASCADE);
 
-insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripción) values (1, 'Coop', 2, 'Busco jugadores para jugar a LOL');
-insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripción) values (2, 'Coop', 2, 'Busco jugadores para jugar a Elden Ring');
-insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripción) values (3, 'Coop', 2, 'Busco jugadores para jugar a Valorant');
-insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripción) values (4, 'PvP', 2, 'Busco jugadores para jugar a Mario Kart');
-insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripción) values (5, 'Coop', 2, 'Busco jugadores para jugar a Age of Empires IV');
+insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripcion) values (1, 'Coop', 2, 'Busco jugadores para jugar a LOL');
+insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripcion) values (2, 'Coop', 2, 'Busco jugadores para jugar a Elden Ring');
+insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripcion) values (3, 'Coop', 2, 'Busco jugadores para jugar a Valorant');
+insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripcion) values (4, 'PvP', 2, 'Busco jugadores para jugar a Mario Kart');
+insert into Grupo (Id_juego, Modo_de_Juego, Dimension, Descripcion) values (5, 'Coop', 2, 'Busco jugadores para jugar a Age of Empires IV');
+
+select * from Grupo;
 
 create table Usuario (
 Nombre_Usuario varchar(20) NOT NULL,
@@ -38,16 +45,18 @@ Email varchar(100) DEFAULT NULL,
 Palabra_clave varchar(100) DEFAULT NULL,
 Usuario_steam varchar(20) DEFAULT NULL,
 Foto_perfil varchar(255),
+Fecha_creación datetime,
+Fecha_modificacion datetime,
 Rol varchar(50),
-Id_grupo int unsigned,
+Id_grupo int,
 PRIMARY KEY(Nombre_Usuario),
 foreign key (Id_grupo) references Grupo(Id_grupo) ON DELETE CASCADE ON UPDATE CASCADE);
 
-insert into Usuario values ('albert', 'Albert', 'Notario Mestres', 'albertnotariomestres@gmail.com', '12345', 'Nones', 'https://logos-marcas.com/wp-content/uploads/2020/12/Danone-Logotipo-2017-presente.jpg', 'user', 1);
+insert into Usuario values ('albert', 'Albert', 'Notario Mestres', 'albertnotariomestres@gmail.com', '12345', 'Nones', 'https://logos-marcas.com/wp-content/uploads/2020/12/Danone-Logotipo-2017-presente.jpg', '1998-01-23 12:45:56', '1998-01-23 12:45:56', 'user', 1);
 
 create table Chat (
 Id_chat int unsigned auto_increment primary key,
-Id_grupo int unsigned,
+Id_grupo int,
 foreign key (Id_grupo) references Grupo(Id_grupo) ON DELETE CASCADE ON UPDATE CASCADE);
 
 insert into Chat (Id_grupo) values (1);
@@ -61,11 +70,11 @@ Hora datetime,
 foreign key (Id_chat) references Chat(Id_chat) ON DELETE CASCADE ON UPDATE CASCADE,
 foreign key (Nombre_Usuario) references Usuario(Nombre_Usuario) ON DELETE CASCADE ON UPDATE CASCADE);
 
-insert into Mensaje (Id_Chat, Usuario, Contenido, Hora) values (1, 'albert', 'hola que tal', '23-01-1998 12:45:56');
+insert into Mensaje (Id_Chat, Nombre_Usuario, Contenido, Hora) values (1, 'albert', 'hola que tal', '1998-01-23 12:45:56');
 
 create table Se_Une (
 Id_seune int unsigned auto_increment,
-Id_grupo int unsigned,
+Id_grupo int,
 Nombre_Usuario varchar(20),
 primary key(Id_seune, Id_grupo, Nombre_Usuario),
 foreign key(Id_grupo) references Grupo(Id_grupo) ON DELETE CASCADE ON UPDATE CASCADE,
