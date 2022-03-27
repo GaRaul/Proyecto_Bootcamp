7f4db2db3e453b2082ea385043e8c1bff2a2a872
Raúl <rgarridorasillo@gmail.com>
2022-03-27 19:52:40 +0200

    Descorrección URL 2

--- src/main/java/main/security/Constants.java
+++ src/main/java/main/security/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 													};
 	public static final String LOGIN_URL = "/login";
 	public static final String REGISTER_URL = "/api/usuarios";
-	public static final String UPDATE_URL = "/api/usuarios/id";
+	public static final String UPDATE_URL = "/api/usuarios/{id}";
 	public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
 	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
 
