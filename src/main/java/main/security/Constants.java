1ed0a5be1fa516de8bde7e859f6c8d22638cd12b
Abel-Git2103 <abel_jornet@hotmail.com>
2022-03-27 17:43:26 +0200

    AJUSTE SEGURIDAD REGISTER

--- src/main/java/main/security/Constants.java
+++ src/main/java/main/security/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 													"/configuration/**"													
 													};
 	public static final String LOGIN_URL = "/login";
-	public static final String REGISTER_URL = "/register";
+	public static final String REGISTER_URL = "/api/usuarios";
 	public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
 	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
 
