8ae8a9e854f792a152ceb51fdae04f45891d3c4e
Abel-Git2103 <abel_jornet@hotmail.com>
2022-03-28 16:32:08 +0200

    TRY CORS

--- src/main/java/main/security/SimpleCORSFilter.java
+++ src/main/java/main/security/SimpleCORSFilter.java
@@ -29,12 +29,13 @@ public class SimpleCORSFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
 		
-		response.setHeader("Access-Control-Allow-Origin", request.getHeader("*"));
-		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers",
-				"access_token, authorization, content-type");
-
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods", "POST, HEAD, PUT, GET, OPTIONS, DELETE");
+		response.addHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers," +
+                "Origin, Accept, X - Requested - With, Content - Type, Access - Control - Request - Method," +
+                "Access - Control - Request - Headers, Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Authorization, Access-Control-Allow-Origin,Access-Control-Allow-Credentials ");
+        
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
