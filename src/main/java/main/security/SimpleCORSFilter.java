b7e8526cf30bb59aa9ff87bc0a545ecf47aa1d00
Abel-Git2103 <abel_jornet@hotmail.com>
2022-03-28 14:21:15 +0200

    CAMBIOS

--- src/main/java/main/security/SimpleCORSFilter.java
+++ src/main/java/main/security/SimpleCORSFilter.java
@@ -31,6 +31,7 @@ public class SimpleCORSFilter implements Filter {
 		
 		response.setHeader("Access-Control-Allow-Origin", request.getHeader("*"));
 		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
 				"Accept, X-Custom-Header, X-Requested-With, remember-me, Host, Content-Lenght");
