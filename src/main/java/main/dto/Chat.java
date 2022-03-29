d09007e6bd57bbe6090184bddfd84984b272e8a2
Raúl <rgarridorasillo@gmail.com>
2022-03-29 17:20:12 +0200

    Constructor ChatDTO

--- src/main/java/main/dto/Chat.java
+++ src/main/java/main/dto/Chat.java
@@ -37,10 +37,13 @@ public class Chat {
 	public Chat() {
 	}
 
-	public Chat(int id_chat) {
+	public Chat(int id_chat, Grupo grupo, List<Mensaje> mensaje) {
+		super();
 		this.id_chat = id_chat;
+		this.grupo = grupo;
+		this.mensaje = mensaje;
 	}
-
+	
 	// Getters and Setters
 	public int getId_chat() {
 		return id_chat;
