eaf47d640d65e296381d8adabe26b3ddc4dca2e6
EdgarFalco <56967437+EdgarFalco@users.noreply.github.com>
2022-03-29 17:42:58 +0200

    CORS FUNCIONANDO

--- src/main/java/main/security/WebSecurity.java
+++ src/main/java/main/security/WebSecurity.java
@@ -64,9 +64,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();		
-		configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT"));
-		
-		 
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT", "DELETE"));		 
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues().combine(configuration));		
 		return source;
