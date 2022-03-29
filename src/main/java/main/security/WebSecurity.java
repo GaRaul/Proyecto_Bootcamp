a8676a92f1e0e14bac95a15aa23355793e035bfb
EdgarFalco <56967437+EdgarFalco@users.noreply.github.com>
2022-03-29 15:55:19 +0200

    cors

--- src/main/java/main/security/WebSecurity.java
+++ src/main/java/main/security/WebSecurity.java
@@ -19,6 +19,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
@@ -61,8 +63,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		CorsConfiguration cc = new CorsConfiguration();
+		cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers","Authorization"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();		
+		source.registerCorsConfiguration("/**", cc);
 		return source;
 	}
 }
