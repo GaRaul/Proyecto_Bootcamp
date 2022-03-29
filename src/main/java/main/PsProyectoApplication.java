9c8d90708d21b32cc3916bcd9e56506555329a0a
Raúl <rgarridorasillo@gmail.com>
2022-03-29 09:09:52 +0200

    CORS Configuration - PsProyectoApplication

--- src/main/java/main/PsProyectoApplication.java
+++ src/main/java/main/PsProyectoApplication.java
@@ -2,12 +2,28 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
 public class PsProyectoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PsProyectoApplication.class, args);
 	}
+	
+    @SuppressWarnings("deprecation")
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 
 }
